6d2c8e4c832c4ed6bade6b70ddc5543b3430ec88
Gabriel-Batista <gabedbatista@gmail.com>
2018-10-30 18:13:03 -0400

    Implements car_controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,34 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+    render json: @cars
+  end
+
+  def show
+    @car = Car.find(params[:id])
+    render json: @car
+  end
+
+  def create
+    @new_car = Car.new(img: params[:img])
+    if @new_car.save
+      render json: @new_car
+    else
+      render json: @new_car.errors
+    end
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    if @car.update(img: params[:img])
+      render json: @car
+    else
+      render json: @car.errors
+    end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+  end
 end
