5a2c532d6e20ff33cf119e76e73ccfef2cd91419
Gabriel-Batista <gabedbatista@gmail.com>
2018-11-02 19:42:30 -0400

    Adds complete column to match end point

---
+++ db/migrate/20181102234040_add_complete_to_match.rb
@@ -0,0 +1,5 @@
+class AddCompleteToMatch < ActiveRecord::Migration[5.2]
+  def change
+    add_column :matches, :complete, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_180521) do
+ActiveRecord::Schema.define(version: 2018_11_02_234040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_11_02_180521) do
     t.integer "seats_taken", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "complete", default: false
   end
 
   create_table "user_cars", force: :cascade do |t|
