be5772de906b295065f05b4882b4973325d61578
Gabriel-Batista <gabedbatista@gmail.com>
2018-11-15 19:42:56 -0500

    adds 2 more user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 User.create(name: "Gabe", email: "gabe@test.com", password: '123')
 User.create(name: "Kevin", email: "kevin@test.com", password: '123')
+User.create(name: "Ben", email: "ben@test.com", password: '123')
+User.create(name: "Dani", email: "dani@test.com", password: '123')
