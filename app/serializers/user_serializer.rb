f6ccfd1dc479f4d175692403dc348343a385b4f5
Gabriel-Batista <gabedbatista@gmail.com>
2018-11-15 19:42:30 -0500

    sends users current match in serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :in_match
+  attributes :id, :name, :email, :in_match, :current_match
+
+  def current_match
+    current_match = object.matches.find_by(complete: false)
+    if current_match
+      return current_match
+    else
+      return nil
+    end
+  end
 end
